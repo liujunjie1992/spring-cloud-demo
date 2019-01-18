13e3824d62ba259d5406a0fb41d96c0ec541fbbf
liujj32 <liujj32@lenovo.com>
2019-01-18 15:47:48 +0800

    conf service eureka

--- SpringCloudDemo/SpringConfigServer/src/main/java/me/junjieis/ConfigServerApplication.java
+++ SpringCloudDemo/SpringConfigServer/src/main/java/me/junjieis/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package me.junjieis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
