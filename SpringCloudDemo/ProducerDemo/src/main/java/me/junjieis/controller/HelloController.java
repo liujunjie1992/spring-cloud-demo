a07631d26a5e4c939aec421c3184c00763f75b53
liujj32 <liujj32@lenovo.com>
2019-01-21 10:32:05 +0800

    update producer

--- SpringCloudDemo/ProducerDemo/src/main/java/me/junjieis/controller/HelloController.java
+++ SpringCloudDemo/ProducerDemo/src/main/java/me/junjieis/controller/HelloController.java
@@ -1,14 +1,31 @@
 package me.junjieis.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloController {
 
+    @Value("${spring.application.name}")
+    private String serverId;
+
+    @Value("${server.port}")
+    private int port;
+
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
-        return "hello " + name + "，this is first messge";
+        InetAddress addr = null;
+        try {
+            addr = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        String socket = (null != addr ? addr.getHostAddress()  : serverId)+ ":" + port;
+        return "Hello " + name + ", this is a message from " + socket;
     }
 }
