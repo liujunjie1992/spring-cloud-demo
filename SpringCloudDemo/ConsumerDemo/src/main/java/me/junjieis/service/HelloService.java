c0aa8e199ad21049eecd51e1f4e9b115d50e50ed
liujj32 <liujj32@lenovo.com>
2019-01-24 09:32:30 +0800

    sleuth zipkin

--- SpringCloudDemo/ConsumerDemo/src/main/java/me/junjieis/service/HelloService.java
+++ SpringCloudDemo/ConsumerDemo/src/main/java/me/junjieis/service/HelloService.java
@@ -1,10 +1,11 @@
 package me.junjieis.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.sleuth.instrument.web.client.feign.TraceFeignClientAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name= "spring-cloud-producer", fallback = HelloServiceHystrix.class)
+@FeignClient(name= "spring-cloud-producer", fallback = HelloServiceHystrix.class, configuration = TraceFeignClientAutoConfiguration.class)
 public interface HelloService {
     @RequestMapping(value = "/hello")
     public String hello(@RequestParam(value = "name") String name);
